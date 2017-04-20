535b5b0023557ef986edd307a2f8dc808c8181ff
Colin Moore-Hill <itestedthis1@gmail.com>
2017-04-20 09:11:45 +0100

    Updated Tests!

--- src/pages/about/about.spec.ts
+++ src/pages/about/about.spec.ts
@@ -1,22 +1,28 @@
 /**
  * Created by colinmoore-hill on 11/04/2017.
  */
-
 import { ComponentFixture, async } from '@angular/core/testing';
 import { TestUtils }               from '../../test';
 import { AboutPage }          from './about';
-
 let fixture: ComponentFixture<AboutPage> = null;
 let instance: any = null;
-
 describe('Pages: AboutIonic', () => {
-
   beforeEach(async(() => TestUtils.beforeEachCompiler([AboutPage]).then(compiled => {
     fixture = compiled.fixture;
     instance = compiled.instance;
   })));
-
   it('should create the hello ionic page', async(() => {
     expect(instance).toBeTruthy();
   }));
+  it('should return a welcome message', () =>{
+    const expected = "Hello World"
+    let actual = instance.getWelcomeMessage();
+    expect(actual).toBe(expected);
+  });
+  it('should had a main title of Civic Reactor', () => {
+    const expected = ["A", "B", "C"]
+    let actual = instance.getFireBaseArray()
+    expect(actual).toBe(expected);
+  })
+
 });
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
   constructor(public navCtrl: NavController) {
-    let x = "Hello"
-    let y = "world"
-
-    let z = x + " : " + y
-    console.log("message = " + z)
-
   }
+  getWelcomeMessage = () => {
+    return "Hello World"
+  }
+
+  getFireBaseArray = () => ({
+    return: ["X", "Y", "Z"]
+  })
 
 }
