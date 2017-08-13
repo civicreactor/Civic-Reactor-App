e554fb7a6cc301acbc8e282c2a31b0694834ad75
antonio <ammj24@hotmail.com>
2017-08-13 12:43:26 +0100

    deliver object details

--- src/pages/project-detail/project-detail.ts
+++ src/pages/project-detail/project-detail.ts
@@ -1,22 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ProjectData } from '../../providers/project-data'; 
+import * as firebase from 'firebase';
 
-/*
-  Generated class for the ProjectDetail page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-project-detail',
   templateUrl: 'project-detail.html'
 })
 export class ProjectDetailPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  project: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public projectData: ProjectData) {
+    this.project = this.navParams.data;
+  }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProjectDetailPage');
+    // console.log('ionViewDidLoad ProjectDetailPage');
   }
 
 }
--- src/pages/reactor-detail/reactor-detail.ts
+++ src/pages/reactor-detail/reactor-detail.ts
@@ -3,22 +3,18 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { UserData } from '../../providers/user-data';
 
-/*
-  Generated class for the ReactorDetailPage page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-reactor-detail',
   templateUrl: 'reactor-detail.html'
 })
 export class ReactorDetailPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public userData: UserData) {}
+  reactor: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userData: UserData) {
+    this.reactor = this.navParams.data;
+  }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ReactorDetailPage');
+    // console.log('ionViewDidLoad ReactorDetailPage');
   }
 
 }
