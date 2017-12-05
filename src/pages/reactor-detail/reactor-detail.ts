fab3d3b6e99687e5fcf6968d8ebf59ced7ae0a82
mohtadi <teddybak@hotmail.com>
2017-12-05 15:08:50 +0000

    github activity per user and user activity per projects

--- src/pages/reactor-detail/reactor-detail.ts
+++ src/pages/reactor-detail/reactor-detail.ts
@@ -2,21 +2,38 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { UserData } from '../../providers/user-data';
 import {User} from '../../model/user';
+import { Http } from '@angular/http';
+import {GithubService} from "../../services/GithubService";
+
 
 @Component({
   selector: 'page-reactor-detail',
-  templateUrl: 'reactor-detail.html'
+  templateUrl: 'reactor-detail.html',
+  providers: [GithubService]
 })
-export class ReactorDetailPage {
+export class ReactorDetailPage   {
   user: any;
   edit: boolean = false;
   loggedInUser: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public userData: UserData, 
-              public viewCtrl: ViewController) {
+  contributors;
+  shownGroup = null;
+  result = {
+    data: [],
+  };
+  resultado;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userData: UserData,
+              public viewCtrl: ViewController, private http: Http,
+              public githubService:GithubService) {
     this.user = this.navParams.data;
     this.edit = false;
-    this.loggedInUser = (this.userData.user)?this.userData.user.uid:"";
+    this.loggedInUser = (this.userData.user) ? this.userData.user.uid : "";
+
+    this.resultado = githubService.getData();
+
   }
+
 
   ionViewDidLoad() {
     // console.log('ionViewDidLoad ReactorDetailPage');
@@ -34,5 +51,19 @@ export class ReactorDetailPage {
   cancel() {
     this.edit = false;
   }
+
+  toggleGroup(group) {
+    if (this.isGroupShown(group)) {
+      this.shownGroup = null;
+    } else {
+      this.shownGroup = group;
+    }
+  };
+
+  isGroupShown(group) {
+    return this.shownGroup === group;
+  };
+
+
 
 }
